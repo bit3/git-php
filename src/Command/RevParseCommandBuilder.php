a3dec3857bfd74ef697f7affa5297f628b50467e
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-07-07 17:27:02 +0200

    Fix author validation

--- src/Command/RevParseCommandBuilder.php
+++ src/Command/RevParseCommandBuilder.php
@@ -13,6 +13,7 @@
  * @package    bit3/git-php
  * @author     Tristan Lins <tristan@lins.io>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Radek Crlik <nix0@centrum.cz>
  * @copyright  2014 Tristan Lins <tristan@lins.io>
  * @license    https://github.com/bit3/git-php/blob/master/LICENSE MIT
  * @link       https://github.com/bit3/git-php

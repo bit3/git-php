338487df69bfdd269b71501505042598ca930cc3
Sven Baumann <baumann.sv@gmail.com>
2022-12-27 13:45:06 +0100

    We do not replace the prozess command line after create it. This is obsulete

--- src/Command/CommandBuilderTrait.php
+++ src/Command/CommandBuilderTrait.php
@@ -14,7 +14,7 @@
  * @author     Tristan Lins <tristan@lins.io>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2014-2018 Tristan Lins <tristan@lins.io>
+ * @copyright  2014-2022 Tristan Lins <tristan@lins.io>
  * @license    https://github.com/bit3/git-php/blob/master/LICENSE MIT
  * @link       https://github.com/bit3/git-php
  * @filesource
@@ -125,8 +125,6 @@ trait CommandBuilderTrait
         }
 
         $process = new Process($this->arguments, $this->workingDirectory);
-
-        $process->setCommandLine($process->getCommandLine());
 
         return $process;
     }
